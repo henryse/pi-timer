45ba14ace7ac9f6aa75c808172b6f4002e91f483
henryse <henryse@hotwire.com>
2016-08-07 14:17:33 -0700

    Trying to get script to work.

--- src/pi-timer.sh
+++ src/pi-timer.sh
@@ -20,18 +20,24 @@ process_id=$(ps -e | grep pi-timer | awk '{print $1}')
 # The following part carries out specific functions depending on arguments.
 case "$1" in
   start)
-    echo "Starting pi-timer..."
+    echo "Starting pi-timer...";
+    logger -p info "Starting pi-timer...";
     if [ -z "${process_id}" ]; then
-        sudo pi-timer --daemon=true --port=8080 --random=true;
+        logger -p info "pi-timer startup: pi-timer --daemon=true --port=8080 --random=true;";
+        pi-timer --daemon=true --port=8080 --random=true;
     else
         echo "pi-timer is running: ${process_id}";
+        logger -p info "pi-timer is running: ${process_id}";
     fi
     ;;
   stop)
-    echo "Stopping pi-timer..."
+    echo "Stopping pi-timer...";
+    logger -p info "Stopping pi-timer...";
     if [ -z "${process_id}" ]; then
         echo "pi-timer is not running";
+        logger -p info "pi-timer is not running";
     else
+        logger -p info "sudo kill -9 ${process_id};";
         sudo kill -9 ${process_id};
     fi
     ;;
