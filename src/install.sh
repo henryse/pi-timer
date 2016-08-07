de6179e58cd9a09329d2415cebbf6a67e4d8659f
henryse <henryse@hotwire.com>
2016-08-07 13:44:41 -0700

    Missing LSB tags and overrides

--- src/install.sh
+++ src/install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
+sudo update-rc.d -f pi-timer remove
 sudo cp pi-timer.sh /etc/init.d/pi-timer
 sudo chmod 755 /etc/init.d/pi-timer
 sudo update-rc.d pi-timer defaults
-
-# sudo update-rc.d -f pi-timer remove
